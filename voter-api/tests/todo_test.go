27831c053a0b5ed3d48a3844a36ed42621d37be9
vidyakajale <74523779+vidyakajale@users.noreply.github.com>
2024-02-21 22:29:56 -0700

    Update todo_test.go

--- voter-api/tests/todo_test.go
+++ voter-api/tests/todo_test.go
@@ -1,17 +1,18 @@
 package tests
 
 import (
-	"reflect"
+	"log"
+	"os"
 	"testing"
 
 	//aliasing package name
-	//voter "voter-api/voter"
+
 	voter "voter-api/voter"
 
+	fake "github.com/brianvoe/gofakeit/v6" //aliasing package name
+
+	"github.com/go-resty/resty/v2"
 	"github.com/stretchr/testify/assert"
-	//"github.com/go-resty/resty/v2"
-	//"github.com/stretchr/testify/assert"
-	//"github.com/stretchr/testify/assert"
 )
 
 // Note the default file path is relative to the test package location.  The
@@ -19,129 +20,63 @@ import (
 // database file sits.  So to get there we need to back up a directory and
 // then go into the /data directory.  Thus this is why we are setting the
 // default file name to "../data/todo.json"
-/*
+
 var (
 	BASE_API = "http://localhost:1080"
 
 	cli = resty.New()
-)*/
+)
 
-func TestVoterList_GetAllItems(t *testing.T) {
-	type fields struct {
-		VoterListMap voter.VoterMap
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		want    []voter.Voter
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tr := &voter.VoterList{
-				VoterListMap: tt.fields.VoterListMap,
-			}
-			got, err := tr.GetAllItems()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("VoterList.GetAllItems() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("VoterList.GetAllItems() = %v, want %v", got, tt.want)
-			}
-			assert.NoError(t, err, "Error getting item from Voterlist")
-		})
-	}
-	t.Log("Testing Get All Item: ")
-
-	//Use GetAllItem to get list
-
-	count := 1
-
-	assert.Equal(t, count, 1, "Count mateches ")
-}
-
-/*
-func TestNewVoter(t *testing.T) {
-	tests := []struct {
-		name    string
-		want    *VoterListAPI
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewVoter()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewVoter() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewVoter() = %v, want %v", got, tt.want)
-			}
-		})
+func newRandToDoItem(id uint) voter.Voter {
+	return voter.Voter{
+		VoterID:   id,
+		FirstName: fake.Person().FirstName,
+		LastName:  fake.Person().LastName,
 	}
 }
 
-func TestNewVoter(t *testing.T) {
-	VoterList := [] struct {
+func TestVoterList_AddItem(t *testing.T) {
 
+	rsp, err := cli.R().Delete(BASE_API + "/todo")
+	if rsp.StatusCode() != 200 {
+		log.Printf("error clearing database, %v", err)
+		os.Exit(1)
 	}
-	tests := []struct {
-		name    string
-		want    *VoterList
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := NewVoter()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("NewVoter() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewVoter() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
-func TestVoterListAPI_AddVoter(t *testing.T) {
-	type fields struct {
-		voter *voter.VoterList
-	}
-	type args struct {
-		c *fiber.Ctx
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	} {
-	    var items []voter.ToDoItem
-
-		rsp, err := cli.R().SetResult(&items).Get(BASE_API + "/voter")
+	numLoad := 10
+	for i := 1; i <= numLoad; i++ {
+		item := newRandToDoItem(uint(i))
+		rsp, err := cli.R().
+			SetBody(item).
+			Post(BASE_API + "/voter")
 
 		assert.Nil(t, err)
 		assert.Equal(t, 200, rsp.StatusCode())
-
-		assert.Equal(t, 3, len(items))
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			td := &VoterListAPI{
-				voter: tt.fields.voter,
-			}
-			if err := td.AddVoter(tt.args.c); (err != nil) != tt.wantErr {
-				t.Errorf("VoterListAPI.AddVoter() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
 	}
 }
-*/
+
+func TestVoterList_GetAllItems(t *testing.T) {
+
+	var items []voter.Voter
+
+	rsp, err := cli.R().SetResult(&items).Get(BASE_API + "/voter")
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, rsp.StatusCode())
+
+	assert.Equal(t, 10, len(items), "Count mateches ")
+
+}
+
+func TestVoterList_GetItem(t *testing.T) {
+
+	var items voter.Voter
+
+	rsp, err := cli.R().SetResult(&items).Get(BASE_API + "/voter/7")
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, rsp.StatusCode())
+	assert.Equal(t, uint(7), items.VoterID)
+	log.Printf("Get voter with ID 7, %v", items)
+
+}
